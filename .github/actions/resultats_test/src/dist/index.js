6fa2c2205f0bd7de255e66b113ad588f9df535ac
Alejandro Merenciano <alemeco2005@gmail.com>
2024-12-13 09:51:01 +0100

    Intento 66

--- .github/actions/resultats_test/src/dist/index.js
+++ .github/actions/resultats_test/src/dist/index.js
@@ -27539,44 +27539,65 @@ module.exports = parseParams
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-const fs = __nccwpck_require__(9896);
 const core = __nccwpck_require__(7484);
+const fs = (__nccwpck_require__(9896).promises);
+const path = __nccwpck_require__(6928);
 
-async function run() {
-  const cypressOutcome = core.getInput('cypress_outcome');
-  const badgeUrl = cypressOutcome === 'success'
-    ? 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg'
-    : 'https://img.shields.io/badge/test-failure-red';
+async function main() {
+    try {        
+        console.log('Starting the script...');
+        console.log('Mostrem el directori actual de treball:', process.cwd());
 
-  const readmePath = './README.md';
-  const oldReadmePath = './OldREADME.md'; // Copia de seguretat del README
+        // //Obtinguem el resultat del test.
+        // const resultat_test = core.getInput('test_result');
+        // //Guardem l´insignia de error en cas de que el test falle.
+       
+        // //Guardem l´insignia que mostrarem al readme depenent del resultat del test.
+        // //Guardem el missatge que afegirem abans del l´insignia al readme.
+        // const missatge_badge = `RESULTAT DELS ÚLTIMS TESTS \n ![Test result badge](${badge})`;
 
-  // Llegir el README original i la seva còpia de seguretat
-  let readmeContent = fs.readFileSync(readmePath, 'utf8');
-  const oldReadmeContent = fs.readFileSync(oldReadmePath, 'utf8');
+        // const oldReadmePath = './OldREADME.md';
+        // let oldReadmeContent = await fs.readFile(oldReadmePath, 'utf-8');
+        // let newReadmeContent = oldReadmeContent + "\n" + missatge_badge;
+        const readmePath = path.resolve('./README.md');
+        const resultats_test = core.getInput('resultats_test');
 
-  // Comprovar si el text "RESULTAT DELS ÚLTIMS TESTS" existeix
-  const resultText = 'RESULTAT DELS ÚLTIMS TESTS';
-  const index = readmeContent.indexOf(resultText);
+        const img_error = 'https://img.shields.io/badge/test-failure-red';
+        //Guardem l´insignia del test en cas de que el test resulte exitós.
+        const img_exit = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg';
 
-  if (index !== -1) {
-    // Substituir la secció de RESULTAT DELS ÚLTIMS TESTS amb el badge
-    const newReadmeContent = readmeContent.slice(0, index + resultText.length) +
-      `\n![Test Badge](${badgeUrl})\n` +
-      readmeContent.slice(index + resultText.length);
+        const badge = resultats_test == 'success' ? img_exit : img_error;
 
-    // Escriure els canvis al README.md
-    fs.writeFileSync(readmePath, newReadmeContent, 'utf8');
-    core.info('Badge afegit al README.md');
-  } else {
-    core.setFailed('No s\'ha trobat la secció "RESULTAT DELS ÚLTIMS TESTS" al README.md');
-  }
-}
+        fs.readFile(readmePath, 'utf8', function (err, data) {
+            if (err) throw err;
+            let README = data.search(img_exit) !== -1 ? data.replace(img_exit, badge) : date.replace(img_error, badge);
+            fs.writeFile(readmePath, README, function (err) {
+                if (err) throw err;
+                console.log('Bagde añadido correctamente');
+                process.exit(0);
+            });
+                
+            
+        });
 
-run().catch(error => {
-  core.setFailed(error.message);
-});
 
+
+        // console.log(`Attempting to read: ${oldReadmePath}`);
+        // console.log('Successfully read OldREADME.md');
+        // console.log(`Old README content:\n${oldReadmeContent}`);
+        // console.log(`New README content:\n${newReadmeContent}`);
+        // console.log(`Attempting to write to: ${readmePath}`);
+        // console.log('Successfully wrote to README.md');
+
+        process.exit(0);
+    } catch (e) {
+        console.error(e);
+        core.setFailed(e.message);
+    }
+
+};
+
+main();
 module.exports = __webpack_exports__;
 /******/ })()
 ;
